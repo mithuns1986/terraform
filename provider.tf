fe714fd86230097a8f51815b84cab431e5cad793
Mithun <mithun.s@sgtradex.com>
2023-06-14 13:11:30 +0530

    modules project to create vpc & subnets

--- provider.tf
+++ provider.tf
@@ -7,7 +7,9 @@ terraform {
      }
    backend "s3" {
      region         = "us-east-1"
+     #region = var.aws_region
      bucket         = "mtt-aws-devops-april-2023"
+     #bucket         = var.bucket
      key            = "terraform.tfstate"
      dynamodb_table = "tf-db-lock"
      #profile        = ""
